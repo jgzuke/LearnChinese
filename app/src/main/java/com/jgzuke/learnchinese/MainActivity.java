23f702287aa4b05601b15d275129b3a54ba67d97
jgzuke <jgzuke@gmail.com>
2015-06-19 00:14:15 -0400

    more button functionality, icons

--- app/src/main/java/com/jgzuke/learnchinese/MainActivity.java
+++ app/src/main/java/com/jgzuke/learnchinese/MainActivity.java
@@ -5,34 +5,52 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.gc.materialdesign.views.ButtonFloat;
+import com.jgzuke.learnchinese.translatespeak.Language;
+import com.jgzuke.learnchinese.translatespeak.Translator;
 
 /**
  * Created by jgzuke on 15-06-18.
  */
-public class MainActivity extends ActionBarActivity implements OnClickListener {
+public class MainActivity extends ActionBarActivity implements OnClickListener, TextWatcher {
     private static final String LOG_TAG = "MainActivity";
+
+    private static final int mIconPlayID = R.drawable.ic_play_arrow_white_36dp;
+    private static final int mIconPauseID = R.drawable.ic_pause_white_36dp;
+    private static final int mIconRecordID = R.drawable.ic_mic_white_36dp;
+    private static final int mIconStopRecordID = R.drawable.ic_stop_white_36dp;
+    private static final int mForwardIcon = R.drawable.ic_arrow_forward_white_24dp;
+    private static final int mCheckIcon = R.drawable.ic_check_white_24dp;
 
     private ButtonFloat mRecordBtn;
     private ButtonFloat mPlayRecordingBtn;
     private ButtonFloat mPlayCorrectBtn;
+    private ButtonFloat mTranslateBtn;
+
+    private EditText mTranslateInput;
+    private TextView mTranslateOutput;
+
+    private String mTranslateInputString;
+    private String mTranslateOutputString;
+
+    private boolean mHasTranslated;
 
     private boolean mIsRecording = false;
     private boolean mIsPlayingRecording = false;
     private boolean mIsPlayingCorrect = false;
     private MyAudioRecorder mAudioRecorder;
-
-    private int mIconPlayID = R.drawable.ic_play_arrow_white_36dp;
-    private int mIconPauseID = R.drawable.ic_pause_white_36dp;
-    private int mIconRecordID = R.drawable.ic_mic_white_36dp;
-    private int mIconStopRecordID = R.drawable.ic_stop_white_36dp;
 
     public Resources resources;
 
@@ -43,12 +61,19 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
         resources = getResources();
 
         setContentView(R.layout.activity_main);
+
         mRecordBtn = (ButtonFloat) findViewById(R.id.menu_record_btn);
         mPlayRecordingBtn = (ButtonFloat) findViewById(R.id.menu_recording_btn);
         mPlayCorrectBtn = (ButtonFloat) findViewById(R.id.menu_correct_btn);
+        mTranslateBtn = (ButtonFloat) findViewById(R.id.menu_translate_btn);
+        mTranslateInput = (EditText) findViewById(R.id.translate_input);
+
         mRecordBtn.setOnClickListener(this);
         mPlayRecordingBtn.setOnClickListener(this);
         mPlayCorrectBtn.setOnClickListener(this);
+        mTranslateBtn.setOnClickListener(this);
+        mTranslateInput.addTextChangedListener(this);
+
         mAudioRecorder = new MyAudioRecorder(this);
     }
 
@@ -83,7 +108,29 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
             case R.id.menu_correct_btn:
                 clickPlayCorrect();
                 break;
+            case R.id.menu_translate_btn:
+                translateText();
+                break;
         }
+    }
+
+    private void translateText() {
+        if(mHasTranslated == true) {
+            return;
+        }
+        mHasTranslated = true;
+        setFloatButtonView(mTranslateBtn, R.color.app_blue, mCheckIcon);
+        mTranslateOutputString = Translator.translate(Language.ENGLISH, Language.CHINESE, mTranslateInputString);
+        mTranslateOutput.setText(mTranslateOutputString);
+    }
+
+    private void invalidateText(String translationInput) {
+        mTranslateInputString = translationInput;
+        if(mHasTranslated == false) {
+            return;
+        }
+        mHasTranslated = false;
+        setFloatButtonView(mTranslateBtn, R.color.recording_red, mForwardIcon);
     }
 
     private void clickRecord() {
@@ -139,4 +186,15 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
         button.setBackgroundColor(resources.getColor(colorID));
         button.getIcon().setImageResource(drawableID);
     }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        invalidateText((String) s);
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {}
 }
